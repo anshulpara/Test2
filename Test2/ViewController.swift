3b635e993b690ca790124e4565577b1dcb16f3ac
anshul <anshul@rajasoftwarelabs.com>
2019-08-20 18:38:40 +0530

    part 4

--- Test2/ViewController.swift
+++ Test2/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        print("TEMP: Test2 repo print 2")
+        print("TEMP: Test2 repo print 3")
 
     }
 
